fd3f562a4c0ea9e865b266f846e31c5b5bde26c1
ConstantinKushnir1 <98741160+ConstantinKushnir1@users.noreply.github.com>
2022-04-03 07:44:44 +0300

    lesson-8

---
+++ L8_T1.py
@@ -0,0 +1,42 @@
+class Data:
+    def __init__(self, day_month_year):
+        # self.day = day
+        # self.month = month
+        # self.year = year
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-': my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2022 >= year >= 0:
+                    return f'All right'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+today = Data('03 - 04 - 2022')
+print(today)
+print(Data.valid(3, 4, 2023))
+print(today.valid(38, 11, 2018))
+print(Data.valid(13, 13, 2013))
+print(Data.extract('23 - 12 - 2021'))
+print(today.extract('08 - 08 - 1964'))
+print(Data.valid(1, 11, 2000))
